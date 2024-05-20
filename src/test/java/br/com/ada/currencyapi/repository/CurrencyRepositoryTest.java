f553463f9cacc11d1c7662d754e5ee2d92ae8d74
Daniel <daniel.m.adr@hotmail.com>
2024-05-20 15:26:06 -0300

    test: implement coverage test

--- src/test/java/br/com/ada/currencyapi/repository/CurrencyRepositoryTest.java
+++ src/test/java/br/com/ada/currencyapi/repository/CurrencyRepositoryTest.java
@@ -1,46 +1,40 @@
 package br.com.ada.currencyapi.repository;
 
 import br.com.ada.currencyapi.domain.Currency;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
-import java.util.HashMap;
-import static org.mockito.ArgumentMatchers.anyString;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Map;
 
-@ExtendWith(MockitoExtension.class)
+@DataJpaTest
 class CurrencyRepositoryTest {
 
-  @Mock
-  CurrencyRepository currencyRepository;
+  @Autowired
+  private CurrencyRepository currencyRepository;
+
+  @BeforeEach
+  void setUp(){
+    currencyRepository.save(new Currency(1L, "USD", "Dollar", Map.of()));
+  }
 
   @Test
   @DisplayName("Should find a currency by name successfully")
   void findByNameSuccess() {
-    Currency test = new Currency();
-    test.setName("DMA");
-    test.setDescription("Domain Coin");
-    test.setExchanges(new HashMap<>());
 
-    Mockito.when(currencyRepository.findByName(anyString())).thenReturn(test);
-
-    Currency currency = this.currencyRepository.findByName("DMA");
-
-    assertEquals(currency.getName(), "DMA");
-    assertEquals(currency.getDescription(), "Domain Coin");
-    assertEquals(currency.getExchanges(), new HashMap<>());
+    Currency response = currencyRepository.findByName("USD");
+    Assertions.assertEquals(1l, response.getId());
+    Assertions.assertEquals("USD", response.getName());
   }
 
   @Test
   @DisplayName("Should not find a currency by name")
   void findByNameFail() {
-    Mockito.when(currencyRepository.findByName("DMA")).thenReturn(null);
-    Currency currency = this.currencyRepository.findByName("DMA");
-    assertNull(currency);
+    Currency response = currencyRepository.findByName("BRL");
+    Assertions.assertNull(response);
   }
 
 }
